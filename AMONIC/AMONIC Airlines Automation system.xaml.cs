ab04f59804b7a58337bbdf021b01ae6644cb97d2
Punkc4ke <alex50622@yandex.ru>
2023-03-01 16:51:52 +0300

    first commit

---
+++ AMONIC/AMONIC Airlines Automation system.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AMONIC
+{
+    /// <summary>
+    /// Логика взаимодействия для AMONIC_Airlines_Automation_system.xaml
+    /// </summary>
+    public partial class AMONIC_Airlines_Automation_system : Window
+    {
+        public AMONIC_Airlines_Automation_system()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AMONIC/Login.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Threading;
+
+namespace AMONIC
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        class ConnectString
+        {
+            public string connectionString = ConfigurationManager.ConnectionStrings["AMONIC.Properties.Settings.AMONICConnectionString"].ConnectionString;
+        }
+
+        ConnectString cs = new ConnectString();
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text.Trim() != "" && Password.Password.Trim() != "")
+            {
+                using (SqlConnection connection = new SqlConnection(cs.connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand($"SELECT * FROM [dbo].[Users] WHERE Email = '{Username.Text}' AND Password = '{Password.Password}'", connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader["RoleID"] = 1)
+                        {
+                            AMONIC_Airlines_Automation_system window = new AMONIC_Airlines_Automation_system();
+                            window.Show();
+                            this.Hide();
+                        }                     
+                    }
+                }
+            }
+            else MessageBox.Show("Ошибка. Пустые поля!");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
